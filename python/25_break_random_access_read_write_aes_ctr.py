52bd0e9925e4ae70f8aac5abbc7bc9875bf8b384
J Pomianek <jpomianek@localhost>
2017-03-09 11:32:03 -0800

    25 works on first block only

--- python/25_break_random_access_read_write_aes_ctr.py
+++ python/25_break_random_access_read_write_aes_ctr.py
@@ -9,6 +9,8 @@ from os.path import isfile
 splitbytes = lambda a,b : [a[i:i+b] for i in range(0,len(a),b)]
 xor_bytearrays = lambda a, b: bytes(map(lambda x, y: x ^ y, a, b))
 
+stored_key = 'super$$$$randomm'
+
 def aes_ecb_dec(data, key):
   obj = AES.new(key, AES.MODE_ECB)
   return obj.decrypt(bytes(data))
@@ -17,38 +19,33 @@ def aes_ecb_enc(data, key):
   obj = AES.new(key, AES.MODE_ECB)
   return obj.encrypt(bytes(data))
 
-def aes_ctr(string,key,nonce):
+def aes_ctr(string,nonce):
   arr = splitbytes(string,16)
   result = bytearray()
   for count in range(0,len(arr)):
-    out = aes_ecb_enc(struct.pack('ll',nonce,count),key)
+    out = aes_ecb_enc(struct.pack('ll',nonce,count),stored_key)
     out = out[:len(arr[count])]
     result += xor_bytearrays(arr[count],out)
   return result
 
-def edit_ctr(ciphertext,key,offset,newtext):
+def edit_ctr(ciphertext,offset,newtext):
   nonce = 0
   arr = splitbytes(ciphertext,16)     
-  print("arr0:", arr[0])
   startblock = int(offset/16) * 16
   endblock = int((offset+len(newtext))/16) + 1
-  print("startblock:",startblock,"endblock:",endblock)
   dec = bytes() 
   # decrypt affected blocks
   for count in range(startblock,endblock):
-    enc = aes_ecb_enc(struct.pack('ll',nonce,count),key)
+    enc = aes_ecb_enc(struct.pack('ll',nonce,count),stored_key)
     enc = enc[:len(arr[count])]
     dec += xor_bytearrays(arr[count],enc)
-
-  print("arr0:", arr[0])
   # splice in our substitution text
-  dec = dec[0:offset] + bytes(newtext,'utf-8') + dec[offset + len(newtext):]
+  dec = dec[:offset] + bytes(newtext,'utf-8') + dec[offset + len(newtext):]
   dec_split = splitbytes(dec,16)
-  print("dec0:",dec_split[0])
 
   # re-encrypt
   for count in range(startblock,endblock):
-    enc = aes_ecb_enc(struct.pack('ll',nonce,count),key)
+    enc = aes_ecb_enc(struct.pack('ll',nonce,count),stored_key)
     enc = enc[:len(arr[count])]
     arr[count] = xor_bytearrays(dec_split[count],enc)
   
@@ -65,8 +62,13 @@ key = 'YELLOW SUBMARINE'
 ciphertext = base64.b64decode(''.join(lines))
 
 plaintext = aes_ecb_dec(ciphertext, key)
+original_crypted = aes_ctr(plaintext,0)
 
-ct_ctr = aes_ctr(plaintext,key,0)
+for d in range(0,16):
+  for i in range(0,128):
+    edited = edit_ctr(original_crypted,d,chr(i))
+    if original_crypted[d] == edited[d]:
+      print(chr(i), end="")
 
-edited = edit_ctr(ct_ctr,key,5,'blal')
-print(edited)
+print()
+#  print(edited[0])
